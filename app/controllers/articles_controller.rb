ac1bd0ea306ce5bfe20c6f43d6837282c3d7d273
Ognjen Blazic <ognjen.blazic.dev@gmail.com>
2020-05-24 10:06:53 +0200

    Basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "ogi", password: "pwd", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
